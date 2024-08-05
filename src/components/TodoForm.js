0474a446c9f9f5fb01c7674ea2b0687e3863b825
Somi Yoo <79749558+blue-ssom@users.noreply.github.com>
2024-08-05 18:16:18 +0900

    feat : add TodoForm component and integrate with TodoWrapper

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Todoform = () => {
+// TodoForm 컴포넌트 선언
+export const TodoForm = ({addTodo}) => {
+  // useState 훅을 사용하여 value 상태를 관리. 초기값은 빈 문자열
+  const [value, setValue] = useState("");
+
+  // 폼 제출 시 호출되는 함수
+  const handleSubmit = e => {
+    // 폼의 기본 제출 동작을 막음
+    e.preventDefault();
+
+    // 현재 입력된 값을 콘솔에 출력
+    // console.log(value);
+
+    // addTodo 함수를 호출하여 부모 컴포넌트(TodoWrapper)로 값 전달
+    addTodo(value)
+  };
+
+  // TodoForm 컴포넌트의 렌더링 부분
   return (
-    <div>
-      
-    </div>
-  )
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      {/* 텍스트 입력 필드. 값이 변경될 때마다 setValue 함수 호출 */}
+      <input
+        type="text"
+        className='todo-input'
+        placeholder='What is the task today?'
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {/* 제출 버튼 */}
+      <button type='submit' className='todo-btn'>Add Task</button>
+    </form>
+  );
 }
 
-export default Todoform
+// TodoForm 컴포넌트를 기본으로 내보냄
+export default TodoForm;
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TodoForm } from './TodoForm';
+import { v4 as uuidv4 } from "uuid";  // 유일한 ID 생성을 위한 uuid 라이브러리 사용
 
+// TodoWrapper 컴포넌트 선언
 const TodoWrapper = () => {
+  // useState 훅을 사용하여 todos 상태를 관리. 초기값은 빈 배열
+  const [todos, setTodos] = useState([]);
+
+  // 새로운 todo를 추가하는 함수
+  const addTodo = todo => {
+    // 새로운 todo 객체를 만들고 기존의 todos 배열에 추가
+    setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
+    // 현재 todos 배열을 콘솔에 출력
+    console.log(todos);
+  };
+
+  // TodoWrapper 컴포넌트의 렌더링 부분
   return (
-    <div>TodoWrapper</div>
-  )
+    <div className='TodoWrapper'>
+      {/* TodoForm 컴포넌트를 렌더링하고 addTodo 함수를 props로 전달 */}
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
 }
 
-export default TodoWrapper
+// TodoWrapper 컴포넌트를 기본으로 내보냄
+export default TodoWrapper;
